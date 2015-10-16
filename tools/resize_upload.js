bc3e3d37c5b4bf7bccfade08b188d7a738d0cd81
Hareesh Nagarajan <hareesh.nagarajan@gmail.com>
2015-10-16 15:52:22 -0700

    fix somethings

--- tools/resize_upload.js
+++ tools/resize_upload.js
@@ -28,6 +28,7 @@ function upload(img) {
   imgur.setClientID(ops.api_key);
   imgur.upload(img, function (err, res) {
     console.log('Uploaded to ' + res.data.link);
+
     var cmd = 'curl --data "text=' + ops.quote + '&&imageuri=' + res.data.link +
               '&&category=' + ops.category + '&&submitkey=' + ops.submitkey +
               '" http://45.55.216.153:3000/submit';
@@ -45,28 +46,27 @@ function resize(fname, width_int, height_int, callback) {
   extension = fname.substr(fname.lastIndexOf('.') + 1);
   out_fname = strip_extension + '_' + width + '_' + height + '.' + extension;
   gm(fname)
-    .resize('100', width, '^')
+    .resize(width_int, height_int, '^')
     .gravity('Center')
-    .crop(width, height)
+    .extent(width_int, height_int)
     .noProfile()
     .write(out_fname, function (err) {
       if (!err) {
         console.log('Converted ' + ops.img + ' to ' + out_fname);
+        gm(out_fname).size(function(err, value) {
+          if (!err) {
+            console.log('width = ' + value.width + ' height = ' + value.height);
+            if (value.width == ops.width && value.height == ops.height) {
+              console.log('Converted to size requested. Uploading to imgur');
+              callback(out_fname);
+            } else {
+              console.log('Unable to covert to size requested. Exiting');
+              process.exit(1);
+            }
+          }
+        });
       } else {
         console.log(err);
-      }
-    });
-
-    gm(out_fname).size(function(err, value) {
-      if (!err) {
-        console.log('width = ' + value.width);
-        console.log('height = ' + value.height);
-        if (value.width == ops.width && value.height == ops.height) {
-          console.log('Converted the file to a size requested. Now uploading to imgur ...');
-          callback(out_fname);
-        } else {
-          process.exit(1);
-        }
       }
     });
 };
