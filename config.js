6ceba6959e6b079eac1fb4e16e214160860e3b68
nearfield02 <memristanceisfutile@gmail.com>
2015-12-09 00:28:03 -0600

    Change push notification time to 1PM, 9PM, 5PM and 1AM EST

--- config.js
+++ config.js
@@ -3,10 +3,12 @@ var config = {};
 config.port = 3000;
 config.submitkey = "hellno";
 config.pushnotificationkey = "inyoface";
-//config.db = "agnitest";
-config.db = "agnitest2";
+config.db = "agnitest";
 config.mailing_list_scheduler_frequency = "0 0 10 * * *";
 config.release_content_scheduler_frequency = "0 0 */3 * * *";
-config.push_notification_scheduler_frequency = "10 0 */6 * * *"; /* give ~10 seconds for buffered content to be released */
+
+/* just in case release time overlaps, give ~10 seconds for buffered content
+   to be released  */
+config.push_notification_scheduler_frequency = "10 0 1,13,17,21 * * *";
 
 module.exports = config;
