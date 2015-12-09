82c1b21dbc7f5e09ff93fce263b575ca37b5381a
nearfield02 <memristanceisfutile@gmail.com>
2015-12-09 10:24:27 -0600

    Add aggressive mailing list scheduling interval

--- config.js
+++ config.js
@@ -4,6 +4,11 @@ config.port = 3000;
 config.submitkey = "hellno";
 config.pushnotificationkey = "inyoface";
 config.db = "agnitest";
+
+/* aggressive mailing list scheduler: run mailing list in the
+   0th second of every minute */
+//config.mailing_list_scheduler_frequency = "0 */1 * * * * ";
+
 config.mailing_list_scheduler_frequency = "0 0 10 * * *";
 config.release_content_scheduler_frequency = "0 0 1,3,6,9,13,17,21,23 * * *";
 
