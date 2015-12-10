1321c91b1ff01f1cba83093e2fbd1cad239b7d96
nearfield02 <memristanceisfutile@gmail.com>
2015-12-10 10:38:38 -0600

    changed push notification times to 10AM, 9PM EST

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ config.release_content_scheduler_frequency = "0 0 1,3,6,9,13,17,21,23 * * *";
 
 /* in cases where release time overlaps, give ~10 seconds for buffered content
    to be released  */
-config.push_notification_scheduler_frequency = "10 0 1,13,17,21 * * *";
+config.push_notification_scheduler_frequency = "10 0 10,21 * * *";
 
 module.exports = config;
