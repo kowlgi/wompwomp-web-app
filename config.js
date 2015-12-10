134344662ae0729ac3ee91a94cce585d2f5e4a37
nearfield02 <memristanceisfutile@gmail.com>
2015-12-10 10:40:42 -0600

    changed content push from 9AM -> 10 AM EST:

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config.db = "agnitest";
 //config.mailing_list_scheduler_frequency = "0 */1 * * * * ";
 
 config.mailing_list_scheduler_frequency = "0 0 10 * * *";
-config.release_content_scheduler_frequency = "0 0 1,3,6,9,13,17,21,23 * * *";
+config.release_content_scheduler_frequency = "0 0 1,3,6,10,13,17,21,23 * * *";
 
 /* in cases where release time overlaps, give ~10 seconds for buffered content
    to be released  */
