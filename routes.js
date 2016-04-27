c6446b5185fb19bd855e9ef7e4d049f392e74733
sangamithra kowlgi <tenali@sangamithras-MacBook-Pro.local>
2016-04-27 12:54:54 -0500

    Added function to convert stats to json for aws elastic search

--- routes.js
+++ routes.js
@@ -18,6 +18,7 @@ const Router = Express.Router();
 const Account = App.logindb.model('Accounts');
 const multer  = require('multer');
 const CountryList = require('country-list')();
+const FS = require('fs');
 const upload = multer(
     {
         dest: 'uploads/',
@@ -1455,4 +1456,37 @@ function QueryString(params) {
     return query_string;
 }
 
+function DumpJSONForElasticSearch(){
+    AgniUserStatsModel.
+        find().
+        sort({timestamp: -1}).
+        skip(90000).
+        limit(30000).
+        exec(function(err, result){
+            ESFriendlyJSON = "";
+            for (i = 0; i < result.length; i++){
+              var timestamp = new Date(result[i].timestamp);
+              var geo = geoip.lookup(result[i].ip_address) ||
+                      {city: "XX", region: "XX", country: "XX"};
+              ESFriendlyJSON += "{ \"index\" : { \"_index\" : \"wompers\", \"_type\" : \"wwstat\"} }\n";
+              ESFriendlyJSON += "{ \"ip_address\": \"" + result[i].ip_address +
+                                   "\", \"id\" : \"" + result[i].installation_id +
+                                   "\", \"timestamp\" : \"" + timestamp.toISOString() +
+                                   "\", \"action\" : \"" + result[i].action +
+                                   "\", \"content_id\" : \"" + result[i].content_id +
+                                   "\", \"city\" : \"" + geo.city +
+                                   "\", \"country\" : \"" + CountryList.getName(geo.country) +
+                                   "\"}\n";
+            }
+
+            FS.writeFile("/tmp/wwtest4.json", ESFriendlyJSON, function(err) {
+              if(err) {
+                  return console.log(err);
+              }
+              console.log("The file was saved!");
+            });
+        });
+}
+
 exports.router = Router;
+exports.DumpJSONForElasticSearch = DumpJSONForElasticSearch;
