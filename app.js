e9dbc42549e3adbd66b6241d6dd35e455fdecc7f
rocketsummer <rocketsummer@users.noreply.github.com>
2016-05-27 12:30:49 -0500

    changed colors, icons, favicons, etc

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const app = express();
 const compress = require('compression');
 app.use(compress());
 app.set('port', process.env.PORT || 3000);
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicons', 'favicon.ico')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.locals.moment_local = require('moment-timezone');
