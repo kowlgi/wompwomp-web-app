706a4238cedb05489768cd2d6c0e1e6d55a4e7b8
rocketsummer <rocketsummer@users.noreply.github.com>
2016-05-28 08:44:19 -0500

    Big change: Reduce to 4 items a day and only 1 push notification

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config.sessiondb = "agnisession";
 config.userstatsdb = "agniuserstats";
 config.userretentionstatsdb = "agniuserretentionstats";
 
-config.release_content_scheduler_frequency = "0 0 1,3,7,10,14,17 * * *";
+config.release_content_scheduler_frequency = "0 0 1,3,10,17 * * *";
 config.push_share_card_scheduler_frequency = "10 59 2 * * 1,3,5";
 config.push_rate_card_scheduler_frequency = "10 59 13 * * 0,4";
 config.push_upgrade_card_scheduler_frequency = "30 59 7 * * 0,3,6";
@@ -19,7 +19,7 @@ config.push_upgrade_card_version = "10";
 
 /* in cases where release time overlaps, give ~10 seconds for buffered content
    to be released  */
-config.push_notification_scheduler_frequency = "10 0 3,14 * * *";
+config.push_notification_scheduler_frequency = "10 0 3 * * *";
 
 config.update_featured_items_frequency = "0 0 */1 * * *";
 config.update_user_retention_frequency = "0 0 0 */1 * *"
